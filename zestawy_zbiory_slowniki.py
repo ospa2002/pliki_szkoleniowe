3ff5f6f3fc894446f9eb7775ecf50776a66060fa
Piotr <piotrer2002@gmail.com>
2026-05-12 13:47:40 +0200

    omowione typy danych

--- zestawy_zbiory_slowniki.py
+++ zestawy_zbiory_slowniki.py
@@ -1,2 +1,32 @@
-#zestawy, zbiory i slowniki
+#zestawy, zbiory
 
+naprezenia = [1, 8, 3, 2, 8, 1, 2]
+print(naprezenia)
+
+unikalne = list(set(naprezenia))
+unikalne = unikalne.sort()
+print(unikalne)
+
+#slownik (klucz: wartosc)
+
+dict = { "brand": "Ford", "model": "Mondeo", "year": 2018 }
+print(dict)
+
+x = dict.get("model")
+print(x)
+y = dict.get("model_auta", "brak danych")
+print(y)
+
+dict["year"] = 2023
+dict["kolor"] = "czarny"
+print(dict)
+
+mapa = {(52.1, 21.0): "Warszawa"}
+miejsce = mapa[ (52.1, 21.0)]
+print(miejsce)
+
+klucze = dict.keys()
+print(klucze)
+
+wartosci = dict.values()
+print(wartosci)
